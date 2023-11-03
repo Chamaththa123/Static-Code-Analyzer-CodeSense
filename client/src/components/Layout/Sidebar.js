1de305041c23a60b33bf8ad0a09fa6520a827b7b
Chamaththa Shamod <99489115+Chamaththa123@users.noreply.github.com>
2023-11-03 19:55:12 +0530

    Update Sidebar.js

--- client/src/components/Layout/Sidebar.js
+++ client/src/components/Layout/Sidebar.js
@@ -10,7 +10,7 @@ function Sidebar() {
        <center> <img src={logo} alt='logo' style={{width:'70%'}}/></center>
         <pre className='CodeSense'><i>CodeSense</i></pre>
         <br/>
-  <a href="#about">Analyse Code</a>
+  <a href="/Upload">Analyse Code</a>
   <a href="#services">WCC Metrice</a>
 </div>
     </div>
