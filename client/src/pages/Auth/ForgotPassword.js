da467531f2df573f0be95825ee1d30bad1e9b2af
Chamaththa Shamod <99489115+Chamaththa123@users.noreply.github.com>
2023-11-05 00:04:41 +0530

    forget password

--- client/src/pages/Auth/ForgotPassword.js
+++ client/src/pages/Auth/ForgotPassword.js
@@ -1,18 +1,26 @@
 import React, { useState } from "react";
-import Layout from "./../../components/Layout/Layout";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import Header from "../../components/Layout/Header";
-import Footer from "../../components/Layout/Footer";
+import start from '../../images/start.jpg'
+import logo from '../../images/StaticAnalysis.png'
+import './ForgotPassword.css';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { FiEye, FiEyeOff } from 'react-icons/fi'; // Import eye icons
 
-const ForgotPasssword = () => {
+function ForgotPasssword() {
   const [email, setEmail] = useState("");
   const [newPassword, setNewPassword] = useState("");
   const [answer, setAnswer] = useState("");
-
+  const [showPassword, setShowPassword] = useState(false); // State for password visibility
   const navigate = useNavigate();
 
-  // form function
+  // Function to toggle password visibility
+  function togglePasswordVisibility() {
+    setShowPassword(!showPassword);
+  }
+
+  // Form submission function
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
@@ -33,55 +41,59 @@ const ForgotPasssword = () => {
       window.alert("Something went wrong");
     }
   };
+
   return (
-    <Layout title={"Forgot Password - Ecommerce APP"}>
-      <Header/>
-      <div className="form-container ">
-        <form onSubmit={handleSubmit}>
-          <h4 className="title">RESET PASSWORD</h4>
+    <div style={{ margin: '0', overflow: 'hidden' }}>
+      <img src={start} className='start' />
+      <img src={logo} className='start2' />
 
-          <div className="mb-3">
-            <input
-              type="email"
-              value={email}
+      <div className="button-container111-f">
+        <Form onSubmit={handleSubmit}>
+          <center><pre><h3>Reset Your Password</h3></pre></center>
+          <Form.Group className="mb-3" controlId="formGridEmail">
+            <pre><Form.Label>Email :</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter Email"
+                style={{ fontSize: '13px', padding: '10px' }}
+                value={email}
               onChange={(e) => setEmail(e.target.value)}
-              className="form-control"
-              id="exampleInputEmail1"
-              placeholder="Enter Your Email "
-              required
-            />
-          </div>
-          <div className="mb-3">
-            <input
-              type="text"
-              value={answer}
-              onChange={(e) => setAnswer(e.target.value)}
-              className="form-control"
-              id="exampleInputEmail1"
-              placeholder="Enter Your favorite Sport Name "
-              required
-            />
-          </div>
-          <div className="mb-3">
-            <input
-              type="password"
-              value={newPassword}
+              />
+            </pre>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formGridAddress2">
+            <pre><Form.Label>What is your favorite coding language ?</Form.Label>
+              <Form.Control
+                placeholder="Enter Your Answer"
+                style={{ fontSize: '13px', padding: '10px' }}
+                value={answer}
+                onChange={(e) => setAnswer(e.target.value)}
+              />
+            </pre>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formGridPassword">
+            <pre> <Form.Label>New Password :</Form.Label>
+              <div className="password-input">
+                <Form.Control
+                  type={showPassword ? "text" : "password"} // Show/hide password
+                  placeholder="Enter New Password"
+                  value={newPassword}
               onChange={(e) => setNewPassword(e.target.value)}
-              className="form-control"
-              id="exampleInputPassword1"
-              placeholder="Enter Your Password"
-              required
-            />
-          </div>
-
-          <button type="submit" className="btn btn-primary">
-            RESET
-          </button>
-        </form>
+                  style={{ fontSize: '13px', padding: '10px' }}
+                />
+                <span className="password-toggle" onClick={togglePasswordVisibility} style={{ float: 'right', marginTop: '-8%', color: 'gray', marginRight: '3%' }}>
+                  {showPassword ? <FiEye /> : <FiEyeOff />}
+                </span>
+              </div>
+            </pre>
+          </Form.Group>
+          <pre><Button variant="primary" type="submit" style={{ width: '100%', backgroundColor: '#181833' }}>
+            Reset Passowrd
+          </Button></pre>
+        </Form>
       </div>
-      <Footer/>
-    </Layout>
+    </div>
   );
-};
+}
 
 export default ForgotPasssword;
