bc833fdb14105665f3e0072e453861faed3854cf
Chamaththa Shamod <99489115+Chamaththa123@users.noreply.github.com>
2023-11-06 22:59:38 +0530

    Update reviewRoute.js

--- backend/routes/reviewRoute.js
+++ backend/routes/reviewRoute.js
@@ -3,33 +3,33 @@ import Review from "../models/reviewModel.js"
 
 const router = express.Router();
 
-router.post('/review/add',async(req,res)=>{
+router.post('/review/add', async (req, res) => {
 
-    const name =req.body.name;
-    const rating =req.body.rating;
-    const comment=req.body.comment;
-    const date= req.body.date;
-    
+    const name = req.body.name;
+    const rating = req.body.rating;
+    const comment = req.body.comment;
+    const date = req.body.date;
+
     const newReview = new Review({
-        name,rating,comment,date 
-        })
-    
-        newReview.save((err)=>{
-            if(err){
-                return res.status(400).json({
-                    error: err.message
-                });
-            }
-            return res.status(200).json({
-                success:"review added successfully"
+        name, rating, comment, date
+    })
+
+    newReview.save((err) => {
+        if (err) {
+            return res.status(400).json({
+                error: err.message
             });
+        }
+        return res.status(200).json({
+            success: "review added successfully"
         });
+    });
 
 });
 
 
 
-router.route("/Allcontact").get((req,res) => {
+router.route("/Allcontact").get((req, res) => {
     Contact.find().then((contact) => {
         res.json(contact)
     }).catch((err) => {
@@ -37,7 +37,7 @@ router.route("/Allcontact").get((req,res) => {
     })
 })
 
-router.route("/ContactCount").get((req,res) => {
+router.route("/ContactCount").get((req, res) => {
     Contact.find().count().then((contact) => {
         res.json(contact)
     }).catch((err) => {
@@ -45,53 +45,53 @@ router.route("/ContactCount").get((req,res) => {
     })
 })
 
-router.route("/ReadContactCount").get((req,res) => {
-    Contact.find({status:'Read'}).count().then((contact) => {
+router.route("/ReadContactCount").get((req, res) => {
+    Contact.find({ status: 'Read' }).count().then((contact) => {
         res.json(contact)
     }).catch((err) => {
         console.log(err);
     })
 })
 
-router.route("/UnreadContactCount").get((req,res) => {
-    Contact.find({status:'Unread'}).count().then((contact) => {
+router.route("/UnreadContactCount").get((req, res) => {
+    Contact.find({ status: 'Unread' }).count().then((contact) => {
         res.json(contact)
     }).catch((err) => {
         console.log(err);
     })
 })
 
-router.get('/Contact/:id',(req,res)=>{
+router.get('/Contact/:id', (req, res) => {
     let contactId = req.params.id;
 
-    Contact.findById(contactId,(err,contact) =>{
-        if(err){
-            return res.status(400).json({success:false,err});
+    Contact.findById(contactId, (err, contact) => {
+        if (err) {
+            return res.status(400).json({ success: false, err });
         }
         return res.status(200).json({
-            success:true,
+            success: true,
             contact
         });
     });
 });
 
-router.delete("/contact/delete/:id", async(req,res) =>{
-    let result = await Contact.deleteOne({_id:req.params.id})
+router.delete("/contact/delete/:id", async (req, res) => {
+    let result = await Contact.deleteOne({ _id: req.params.id })
     res.send(result)
 });
 
-router.put('/contact/update/:id',(req,res)=>{
+router.put('/contact/update/:id', (req, res) => {
     Contact.findByIdAndUpdate(
         req.params.id,
         {
-            $set:req.body
+            $set: req.body
         },
-        (err,item)=>{
-            if(err){
-                return res.status(400).json({error:err});
+        (err, item) => {
+            if (err) {
+                return res.status(400).json({ error: err });
             }
             return res.status(200).json({
-                success:"updated successfully"
+                success: "updated successfully"
             });
         }
     );
