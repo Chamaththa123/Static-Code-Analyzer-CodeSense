384649ea1447dff28889ed975d64419490fdaa75
Chamaththa Shamod <99489115+Chamaththa123@users.noreply.github.com>
2023-11-06 22:59:07 +0530

    reviewRoute

---
+++ backend/routes/reviewRoute.js
@@ -0,0 +1,102 @@
+import express from "express";
+import Review from "../models/reviewModel.js"
+
+const router = express.Router();
+
+router.post('/review/add',async(req,res)=>{
+
+    const name =req.body.name;
+    const rating =req.body.rating;
+    const comment=req.body.comment;
+    const date= req.body.date;
+    
+    const newReview = new Review({
+        name,rating,comment,date 
+        })
+    
+        newReview.save((err)=>{
+            if(err){
+                return res.status(400).json({
+                    error: err.message
+                });
+            }
+            return res.status(200).json({
+                success:"review added successfully"
+            });
+        });
+
+});
+
+
+
+router.route("/Allcontact").get((req,res) => {
+    Contact.find().then((contact) => {
+        res.json(contact)
+    }).catch((err) => {
+        console.log(err);
+    })
+})
+
+router.route("/ContactCount").get((req,res) => {
+    Contact.find().count().then((contact) => {
+        res.json(contact)
+    }).catch((err) => {
+        console.log(err);
+    })
+})
+
+router.route("/ReadContactCount").get((req,res) => {
+    Contact.find({status:'Read'}).count().then((contact) => {
+        res.json(contact)
+    }).catch((err) => {
+        console.log(err);
+    })
+})
+
+router.route("/UnreadContactCount").get((req,res) => {
+    Contact.find({status:'Unread'}).count().then((contact) => {
+        res.json(contact)
+    }).catch((err) => {
+        console.log(err);
+    })
+})
+
+router.get('/Contact/:id',(req,res)=>{
+    let contactId = req.params.id;
+
+    Contact.findById(contactId,(err,contact) =>{
+        if(err){
+            return res.status(400).json({success:false,err});
+        }
+        return res.status(200).json({
+            success:true,
+            contact
+        });
+    });
+});
+
+router.delete("/contact/delete/:id", async(req,res) =>{
+    let result = await Contact.deleteOne({_id:req.params.id})
+    res.send(result)
+});
+
+router.put('/contact/update/:id',(req,res)=>{
+    Contact.findByIdAndUpdate(
+        req.params.id,
+        {
+            $set:req.body
+        },
+        (err,item)=>{
+            if(err){
+                return res.status(400).json({error:err});
+            }
+            return res.status(200).json({
+                success:"updated successfully"
+            });
+        }
+    );
+});
+
+
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
-// import codeRoutes from "./routes/codeRoute.js";
+import reviewRoutes from "./routes/reviewRoute.js"
 import cors from "cors";
 import bodyParser from "body-parser";
 
@@ -23,6 +23,7 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 app.use("/api/v1/auth", authRoutes);
+app.use(reviewRoutes);
 
 //rest api
 app.get("/", (req, res) => {
@@ -33,7 +34,7 @@ app.get("/", (req, res) => {
 
 app.use(bodyParser.json());
 //PORT
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 
 //run listen
 app.listen(PORT, () => {
